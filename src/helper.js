ae68c04f30178d2f5a3c495b024bc1f6616aa19e
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:11:59 -0500

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -5,8 +5,11 @@ class Helper {
     }
     
     idKey(){
+        
             var newIDP1 = ((Math.random() * 20) * 30).toPrecision(5);
+        
             var newIDP2 = ((Math.random() * 20) * 30).toPrecision(5);
+        
             var newIDP3 = ((Math.random() * 20) * 30).toPrecision(5);
 
             var newID = `${newIDP1}-${newIDP2}-${newIDP3}`;
@@ -16,13 +19,16 @@ class Helper {
             if(checker !== true){
 
                 this.idList.push(newID);
+                
                 return newID;
 
             }
             else {
+                
                 var newerID = `${newID}-11- '${(Math.random() * 20).toPrecision(5)}'`;
 
                 this.idList.push(newerID);
+                
                 return newerID;
             }       
         }
