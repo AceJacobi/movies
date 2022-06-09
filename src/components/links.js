35504c5c61b0f28d5a56a557585d34c59c157d4f
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 09:59:16 -0500

    Update links.js

--- src/components/links.js
+++ src/components/links.js
@@ -4,20 +4,31 @@ function Links(){
     
     return (
         <div className='links-body'>
-        <h2>Categories</h2> 
-        <div className='link-div'>
-                   
-            <div>
-              <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/action'>Action</Link></p>
-              <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/drama'>Drama</Link></p>
-            </div>
-            <div>
-              <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/scifi'>Sci-Fi</Link></p>
-              <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/comedy'>Comedy</Link></p>
-            </div>            
-            
-            </div>
-        <Link onClick={()=>(window.scrollTo(0,0) )} to='/allmovies'>All Movies</Link>
+        
+            <h2>Categories</h2> 
+
+            <div className='link-div'>
+
+                <div>
+        
+                  <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/action'>Action</Link></p>
+        
+                  <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/drama'>Drama</Link></p>
+                      
+                </div>
+
+                <div>
+                      
+                  <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/scifi'>Sci-Fi</Link></p>
+                      
+                  <p><Link onClick={()=>(window.scrollTo(0,0) )} to='/comedy'>Comedy</Link></p>
+                      
+                </div>            
+
+                </div>
+
+            <Link onClick={()=>(window.scrollTo(0,0) )} to='/allmovies'>All Movies</Link>
+
         </div>
     )
 }
