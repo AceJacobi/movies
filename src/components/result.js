2aef38613e91bda5a1a011cfd392765dfd997fbd
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:04:28 -0500

    Update result.js

--- src/components/result.js
+++ src/components/result.js
@@ -7,33 +7,42 @@ class Result extends React.Component{
         super(props);        
     }
     
-    
-    
     render(){
                 
         var found = this.props.match.params.movie;
         
         var show = this.props.movies.filter((movie)=>(
-        movie.title === found
+            movie.title === found
         ));
                 
         return(
-            <div className='head-space'>            
-            {show.map((mov)=>{            
-            return (
-            <div>
-            <h1>{mov.title}</h1>
-            <hr />
-            <img className='result-img' src={mov.img} alt={mov.title}/>
-            <p>Rating: {mov.rating}</p>
-            <p>Score: {mov.score}</p>
-            <p>{mov.description}</p>
-            </div>
-            )
-        })}
-        <hr />
-    
-        <Links />
+            <div className='head-space'>     
+            
+                {show.map((mov)=>{      
+            
+                    return (
+                        
+                    <div>
+                        
+                        <h1>{mov.title}</h1>
+                        
+                        <hr />
+                        
+                        <img className='result-img' src={mov.img} alt={mov.title}/>
+                        
+                        <p>Rating: {mov.rating}</p>
+        
+                        <p>Score: {mov.score}</p>
+        
+                        <p>{mov.description}</p>
+        
+                    </div>
+                    )})}
+                    
+                <hr />
+
+                <Links />
+                    
             </div>
         )
     }
