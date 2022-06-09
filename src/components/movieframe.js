94a0362bb7922e6044bb8a1a182496c71bac23a6
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:00:03 -0500

    Update movieframe.js

--- src/components/movieframe.js
+++ src/components/movieframe.js
@@ -5,18 +5,16 @@ class MovieFrame extends React.Component{
     constructor(props){
         super(props);
         
-        
         this.takeMe = this.takeMe.bind(this);
     }
     
     takeMe(e){
+        
         var there = e.target.id;
     
         var here = window.location.host;
 
-
-        var nUrl = "/movies/" + there;
-        
+        var nUrl = "/movies/" + there;        
         
         window.scrollTo(0,0);
         
@@ -28,10 +26,15 @@ class MovieFrame extends React.Component{
         
         return (
         <div className='movie-frame-div' onClick={this.takeMe} id={this.props.title}>
+    
             <img className='untouched' src={this.props.img} alt={this.props.title} />
+    
             <p className='untouched'>{this.props.title}</p>       
+   
             <p className='untouched'>{this.props.rating}</p>
+    
             <p className='untouched'>{this.props.score}</p>
+    
         </div>
         )
     }
